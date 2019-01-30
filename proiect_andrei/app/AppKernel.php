e6d4160601157aeb9de0eba6308e3d50560c4fad
Andrei Gache <andrei@AndreiGache.home>
2019-01-30 17:52:55 +0100

    add admin page

--- proiect_andrei/app/AppKernel.php
+++ proiect_andrei/app/AppKernel.php
@@ -17,9 +17,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
             new FrontBundle\FrontBundle(),
-
-
+            new BackOfficeBundle\BackOfficeBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ proiect_andrei/src/BackOfficeBundle/Entity/User.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace BackOfficeBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="fos_user")
+ * @ORM\Entity(repositoryClass="BackOfficeBundle\Repository\UserRepository")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
+
