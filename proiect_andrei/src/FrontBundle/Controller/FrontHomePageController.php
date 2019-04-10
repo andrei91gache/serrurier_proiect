ac7c0b7eec225334498c62d4d959284488391915
Andrei Gache <andrei.gache.99@gmail.com>
2019-04-10 23:16:25 +0200

    send email serrvice update

--- proiect_andrei/src/FrontBundle/Controller/FrontHomePageController.php
+++ proiect_andrei/src/FrontBundle/Controller/FrontHomePageController.php
@@ -24,30 +24,13 @@ class FrontHomePageController extends Controller
 
     public function sendFormAction(Request $request)
     {
-        mail('andrei.gache.99@gmail.com', 'test email', 'hello');
-        $mailer = $this->get('mailer');
-        $message = (new \Swift_Message('Hello Email'))
-            ->setFrom('andrei@andrei-gache.com')
-            ->setTo('andrei.gache.99@gmail.com')
-            ->setBody("sdsddssdsds"
-            )
-        ;
-
-        $s = $mailer->send($message);
-        var_dump($s);
-        die();
-       /* $response = new Response();
+        $response = new Response();
         if($request->isXmlHttpRequest()){
-            //Get Service GoogleHangoutChat
-            $hangoutChat = $this->get('hangout_messaging');
-            //Send message
-            $success = $hangoutChat->sendMessage($request);
-            if ($success)
+            if ($this->get('mail_messaging')->sendMail($request))
                 $response->setStatusCode(Response::HTTP_ACCEPTED);
             else
                 $response->setStatusCode(Response::HTTP_SERVICE_UNAVAILABLE);
-
         }
-        return $response;*/
+        return $response;
     }
 }
---
+++ proiect_andrei/src/Messaging/EmailingBundle/Services/MailService.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andrei Gache
+ * Date: 10/04/2019
+ * Time: 22:30
+ */
+
+namespace Messaging\EmailingBundle\Services;
+
+
+use Symfony\Bridge\Twig\TwigEngine;
+use Symfony\Component\HttpFoundation\Request;
+
+class MailService
+{
+    /**
+     * @var Request $request
+     */
+    private $request;
+
+    /**
+     * @var TwigEngine $templating
+     */
+    private $templating;
+
+    /**
+     * @var string $view_mail
+     */
+    private $view_mail;
+
+    public function __construct(TwigEngine $templating)
+    {
+        $this->templating = $templating;
+    }
+
+    public function sendMail(Request $request){
+        $this->request = $request->request->all();
+        $this->createMessage();
+        return $this->flushMail();
+    }
+
+    private function createMessage()
+    {
+        $this->view_mail =  $this->templating->render('@MessagingEmailing/Email/mail.html.twig', [
+            'name' => trim($this->request['name']),
+            'phone'=> trim($this->request['phone']),
+            'email'=> trim($this->request["email"]),
+            'message'=>trim($this->request["message"])
+        ]);
+    }
+
+    private function flushMail()
+    {
+        return mail('andrei.gache.99@gmail.com', 'Mesaj Website serrurieremetallerie.fr', $this->view_mail);
+    }
+}
